e5f2cbdfc6adec52da0a538e4131dd8e2de99770
ggerritsen <geertgerritsen@gmail.com>
2012-02-11 18:59:12 +0100

    removed (20) from bigint to see whether postgreSQL likes that

--- db/evolutions/1.sql
+++ db/evolutions/1.sql
@@ -2,7 +2,7 @@
 # --- !Ups
 
 CREATE TABLE research_subject (
-    id bigint(20) NOT NULL AUTO_INCREMENT,
+    id bigint NOT NULL AUTO_INCREMENT,
     intelligentie_Bvolgorde int DEFAULT -1,
     intelligentie_Vtijd double DEFAULT -1.0,
     intelligentie_score int DEFAULT -1,
