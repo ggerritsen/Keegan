7035352ddf65ca3acf46809c7dc2c34eed46e093
ggerritsen <geertgerritsen@gmail.com>
2012-02-12 22:13:15 +0100

    Added play evolution for hibernate_sequence, which seems to be necessary

---
+++ db/evolutions/2.sql
@@ -0,0 +1,8 @@
+# sequence for sequence subject_sequence 
+# --- !Ups
+
+CREATE SEQUENCE subject_sequence START 1;
+
+# --- !Downs
+
+DROP SEQUENCE subject_sequence;
---
+++ db/evolutions/3.sql
@@ -0,0 +1,49 @@
+# assessment schema
+# --- !Ups
+
+CREATE TABLE research_subject (
+    id int PRIMARY KEY DEFAULT nextval('subject_sequence'),
+    intelligentie_Bvolgorde int DEFAULT -1,
+    intelligentie_Vtijd real DEFAULT -1.0,
+    intelligentie_score int DEFAULT -1,
+    commercieelInzicht_Bvolgorde int DEFAULT -1,
+    commercieelInzicht_Vtijd real DEFAULT -1.0,
+    commercieelInzicht_score int DEFAULT -1,
+    verkoopVaardigheden_Bvolgorde int DEFAULT -1,
+    verkoopVaardigheden_Vtijd real DEFAULT -1.0,
+    verkoopVaardigheden_score int DEFAULT -1,
+    onderhandelen_Bvolgorde int DEFAULT -1,
+    onderhandelen_Vtijd real DEFAULT -1.0,
+    onderhandelen_score int DEFAULT -1,
+    productkennis_Bvolgorde int DEFAULT -1,
+    productkennis_Vtijd real DEFAULT -1.0,
+    productkennis_score int DEFAULT -1,
+    klantGerichtheid_Bvolgorde int DEFAULT -1,
+    klantGerichtheid_Vtijd real DEFAULT -1.0,
+    klantGerichtheid_score int DEFAULT -1,
+    communicatieveVaardigheid_Bvolgorde int DEFAULT -1,
+    communicatieveVaardigheid_Vtijd real DEFAULT -1.0,
+    communicatieveVaardigheid_score int DEFAULT -1,
+    openheid_Bvolgorde int DEFAULT -1,
+    openheid_Vtijd real DEFAULT -1.0,
+    openheid_score int DEFAULT -1,
+    conscientieusheid_Bvolgorde int DEFAULT -1,
+    conscientieusheid_Vtijd real DEFAULT -1.0,
+    conscientieusheid_score int DEFAULT -1,
+    extraversie_Bvolgorde int DEFAULT -1,
+    extraversie_Vtijd real DEFAULT -1.0,
+    extraversie_score int DEFAULT -1,
+    aangenaamheid_Bvolgorde int DEFAULT -1,
+    aangenaamheid_Vtijd real DEFAULT -1.0,
+    aangenaamheid_score int DEFAULT -1,
+    neuroticisme_Bvolgorde int DEFAULT -1,
+    neuroticisme_Vtijd real DEFAULT -1.0,
+    neuroticisme_score int DEFAULT -1,
+    eerlijkheid_Bvolgorde int DEFAULT -1,
+    eerlijkheid_Vtijd real DEFAULT -1.0,
+    eerlijkheid_score int DEFAULT -1
+);
+
+# --- !Downs
+
+DROP TABLE research_subject;
