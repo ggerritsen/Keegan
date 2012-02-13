6283fcc8ecaf56c97e515ae4375006b0f462d9c3
ggerritsen <geertgerritsen@gmail.com>
2012-02-13 22:16:59 +0100

    Retrieve ResearchSubject by id server-side

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,9 +16,9 @@ public class Application extends Controller {
     }
 
     // called with ajax
-    public static void postAssessmentData(String scaleLabel, int value, String time) {
-        Logger.info(String.format("Incoming assessment data: scale = %s, value = %s, time = %s", scaleLabel, value, time));
-        ResearchSubject subject = new ResearchSubject();
+    public static void postAssessmentData(String scaleLabel, int value, String time, long subjectId) {
+        Logger.info(String.format("Incoming assessment data: scale = %s, value = %s, time = %s, id = %s", scaleLabel, value, time, subjectId));
+        ResearchSubject subject = getSubjectWithId(subjectId);
         Assessment assessment = subject.getAssessmentWithLabel(scaleLabel);
 
         assessment.setTime(time).setValue(value);
@@ -26,5 +26,13 @@ public class Application extends Controller {
 
         subject.save();
         Logger.info(String.format("Stored subject %s", subject));
+    }
+
+    private static ResearchSubject getSubjectWithId(long subjectId) {
+        ResearchSubject subject = ResearchSubject.findById(subjectId);
+        if (subject == null) {
+            subject = new ResearchSubject();
+        }
+        return subject;
     }
 }
