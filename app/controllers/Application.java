01a241675f83c7ad19cd7a06ef4523d5b2d5f1ea
ggerritsen <geertgerritsen@gmail.com>
2012-03-03 14:19:32 +0100

    Use session to store subjectId

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,14 +7,18 @@ import play.mvc.Controller;
 
 public class Application extends Controller {
 
+    private static final String SUBJECT_ID = "subjectId";
+
     public static void index() {
         render();
     }
 
+    // page 1
     public static void start() {
         render();
     }
 
+    // page 2
     public static void instructions(String pp_code, String pp_code_repeated) throws Exception {
         validation.required(pp_code);
         validation.required(pp_code_repeated);
@@ -28,15 +32,19 @@ public class Application extends Controller {
         ResearchSubject subject = new ResearchSubject();
         subject.setPpCode(pp_code);
         subject.save();
+        session.put(SUBJECT_ID, subject.getId());
         Logger.info("Proefpersoon with code %s is stored with id %s", subject.getPpCode(), subject.getId());
-        render(subject);
+        render();
     }
 
-    public static void interview(long subjectId) {
+    // page 4
+    public static void interview() {
+        String subjectId = session.get(SUBJECT_ID);
+        Logger.info("Showing interview page for subject with id %s", subjectId);
         render(subjectId);
     }
 
-    // called with ajax
+    // called with ajax on page 4
     public static void postAssessmentData(String scaleLabel, int value, String time, long subjectId) {
         Logger.info(String.format("Incoming assessment data: scale = %s, value = %s, time = %s, id = %s", scaleLabel, value, time, subjectId));
         ResearchSubject subject = getSubjectWithId(subjectId);
